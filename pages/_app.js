abf2095aa6f4815091833a6fbc8cf537077b6834
Michael Skircak <skircakm@gmail.com>
2021-02-02 20:52:34 -0500

    Start of user auth, github only atm + Firebase db

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { AuthProvider } from '@/lib/auth';
+import { ChakraProvider, CSSReset, extendTheme } from '@chakra-ui/react';
+import theme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
+};
 
-export default MyApp
+export default App;
