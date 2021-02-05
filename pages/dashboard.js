f46aaacb2327425ffe50b6aa50ea2156ff98bfaf
Michael Skircak <michaelskircak@gmail.com>
2021-02-05 18:45:43 -0500

    Swr fbadmin (#6)
    * Fixing API vercel
    
    * Designed and build dashboard with site db
    
    * Adding SWR + firebase admin

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,27 @@
-import Head from 'next/head';
-import { Button, Heading, Text, Code, Icon, Box, Flex } from '@chakra-ui/react';
-
+import useSWR from 'swr';
 import { useAuth } from '@/lib/auth';
 import EmptyState from '@/components/EmptyState';
-
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboardShell';
+import fetcher from '@/utils/fetcher';
+import SiteTable from '@/components/SiteTable';
 const Dashboard = () => {
   const auth = useAuth();
-  if (!auth.user) {
-    return 'Loading...';
+  const { data } = useSWR('/api/sites', fetcher);
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    );
   }
 
-  return <EmptyState />;
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  );
 };
 
 export default Dashboard;
