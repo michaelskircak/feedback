3470da211708bd9cafe4bd2df7e3ccc8866e3662
Michael Skircak <michaelskircak@gmail.com>
2021-02-07 12:17:18 -0500

    Feedback pages (#7)
    * Fixing API vercel
    
    * Designed and build dashboard with site db
    
    * Adding SWR + firebase admin
    
    * Adding feedback pages

---
+++ pages/p/[siteId].js
@@ -0,0 +1,87 @@
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import Feedback from '@/components/Feedback';
+import { useRef, useState } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Box,
+  Input,
+  Button
+} from '@chakra-ui/react';
+import { useAuth } from '@/lib/auth';
+import { createFeedback } from '@/lib/db';
+import { useRouter } from 'next/router';
+
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const { feedback } = await getAllFeedback(siteId);
+
+  return {
+    props: {
+      intialFeedback: feedback
+    }
+  };
+}
+
+export async function getStaticPaths() {
+  const { sites } = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString()
+    }
+  }));
+
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+const SiteFeedback = ({ intialFeedback }) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const inputEl = useRef(null);
+  const [allFeedback, setAllFeedback] = useState(intialFeedback);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      text: inputEl.current.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: 'pending'
+    };
+
+    setAllFeedback([newFeedback, ...allFeedback]);
+    createFeedback(newFeedback);
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxWidth="700px"
+      margin="0 auto"
+    >
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl my={8}>
+          <FormLabel htmlFor="comment">Comment</FormLabel>
+          <Input ref={inputEl} type="comment" id="comment" />
+          <Button mt={2} type="submit" fontWeight="medium">
+            Add Comment
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback.map((feedback) => (
+        <Feedback key={feedback.id} {...feedback} />
+      ))}
+    </Box>
+  );
+};
+
+export default SiteFeedback;
