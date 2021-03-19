d37beafe7e0ae0b68e7a004e4064b11f8ee9e032
Michael Skircak <michaelskircak@gmail.com>
2021-03-18 22:23:16 -0400

    Fix revalidate (#15)
    * Stripe integration and bugs
    
    * vercel bug env missing
    
    * fixed revalidate issue with vercel

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -1,61 +1,65 @@
 import React from 'react';
 import NextLink from 'next/link';
-import {
-  ChakraProvider,
-  Flex,
-  Link,
-  Stack,
-  Icon,
-  Avatar,
-  Button
-} from '@chakra-ui/react';
+import { Box, Button, Flex, Link, Avatar, Icon } from '@chakra-ui/react';
+
 import { useAuth } from '@/lib/auth';
 
 const DashboardShell = ({ children }) => {
-  const { user, signout } = useAuth();
+  const { user } = useAuth();
+
   return (
-    <ChakraProvider resetCSS>
-      <Flex flexDirection="column">
+    <Box backgroundColor="gray.100" h="100vh">
+      <Flex
+        backgroundColor="white"
+        mb={16}
+        w="full"
+        borderTop="5px solid #0AF5F4"
+      >
         <Flex
-          flexDirection="row"
-          backgroundColor="whiteAlpha.500"
-          justifyContent="space-between"
           alignItems="center"
-          p={4}
+          justifyContent="space-between"
+          pt={4}
+          pb={4}
+          maxW="1250px"
+          margin="0 auto"
+          w="full"
           px={8}
+          h="70px"
         >
-          <Stack spacing={4} isInline alignItems="center">
+          <Flex align="center">
             <NextLink href="/" passHref>
-              <Icon viewBox="0 0 46 32" color="black" boxSize="24px">
-                <path
-                  fill="currentColor"
-                  d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
-                />
-              </Icon>
+              <Link>
+                <Icon viewBox="0 0 46 32" color="black" boxSize="24px">
+                  <path
+                    fill="currentColor"
+                    d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
+                  />
+                </Icon>
+              </Link>
             </NextLink>
             <NextLink href="/dashboard" passHref>
-              <Link>Sites</Link>
+              <Link mr={4}>Sites</Link>
             </NextLink>
             <NextLink href="/feedback" passHref>
               <Link>Feedback</Link>
             </NextLink>
-          </Stack>
-          <Flex alignItems="center">
+          </Flex>
+          <Flex justifyContent="center" alignItems="center">
             {user && (
-              <Button variant="ghost" mr={2} onClick={() => signout()}>
-                Log Out
-              </Button>
+              <NextLink href="/account" passHref>
+                <Button as="a" variant="ghost" mr={2}>
+                  Account
+                </Button>
+              </NextLink>
             )}
             <Avatar size="sm" src={user?.photoUrl} />
           </Flex>
         </Flex>
-        <Flex backgroundColor="gray.100" p={8} height="100vh">
-          <Flex maxWidth={1250} w="100%" direction="column" ml="auto" mr="auto">
-            {children}
-          </Flex>
-        </Flex>
       </Flex>
-    </ChakraProvider>
+      <Flex margin="0 auto" direction="column" maxW="1250px" px={8}>
+        {children}
+      </Flex>
+    </Box>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,94 +1,116 @@
 import Head from 'next/head';
-import { Button, Flex, Text, Stack } from '@chakra-ui/react';
+import { Box, Button, Flex, Text, Stack } from '@chakra-ui/react';
 
 import { useAuth } from '@/lib/auth';
+import { getAllFeedback } from '@/lib/db-admin';
+import Feedback from '@/components/Feedback';
+import FeedbackLink from '@/components/FeedbackLink';
 
-const Home = () => {
+const SITE_ID = '6KXYn5DPlcg4SNoFy1mV';
+
+export async function getStaticProps(context) {
+  const { feedback } = await getAllFeedback(SITE_ID);
+
+  return {
+    props: {
+      allFeedback: feedback
+    },
+    revalidate: 1
+  };
+}
+
+const Home = ({ allFeedback }) => {
   const auth = useAuth();
 
   return (
-    <Flex
-      as="main"
-      direction="column"
-      align="center"
-      justify="center"
-      h="100vh"
-      maxW="400px"
-      margin="0 auto"
-    >
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                if (document.cookie && document.cookie.includes('feedback-auth')) {
-                  window.location.href = "/dashboard"
-                }
-              `
-          }}
-        />
-        <title>Feedback</title>
-      </Head>
+    <>
+      <Box bg="gray.100" py={16}>
+        <Flex as="main" direction="column" maxW="700px" margin="0 auto">
+          <Head>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              if (document.cookie && document.cookie.includes('fast-feedback-auth')) {
+                window.location.href = "/dashboard"
+              }
+            `
+              }}
+            />
+            <title>Feedback</title>
+          </Head>
 
-      <Text mb={4} fontSize="lg" p={6}>
-        <Text as="span" fontWeight="bold" display="inline">
-          Feedback
-        </Text>
-        {`. It's the easiest way to add comments or reviews to your static site.`}
-      </Text>
-      {auth.user ? (
-        <Button
-          as="a"
-          mt={4}
-          href="/dashboard"
-          size="lg"
-          backgroundColor="white"
-          color="gray.900"
-          variant="outline"
-          fontWeight="medium"
-          _hover={{ bg: 'gray.100' }}
-          _active={{
-            bg: 'gray.100',
-            transform: 'scale(0.95)'
-          }}
-        >
-          View Dashboard
-        </Button>
-      ) : (
-        <Stack>
-          <Button
-            mt={4}
-            size="lg"
-            onClick={(e) => auth.signinWithGitHub()}
-            backgroundColor="gray.900"
-            color="white"
-            fontWeight="medium"
-            _hover={{ bg: 'gray.700' }}
-            _active={{
-              bg: 'gray.800',
-              transform: 'scale(0.95)'
-            }}
-          >
-            Sign In with Github
-          </Button>
-          <Button
-            mt={4}
-            size="lg"
-            onClick={(e) => auth.signinWithGoogle()}
-            backgroundColor="white"
-            color="gray.900"
-            variant="outline"
-            fontWeight="medium"
-            _hover={{ bg: 'gray.100' }}
-            _active={{
-              bg: 'gray.100',
-              transform: 'scale(0.95)'
-            }}
-          >
-            Sign In with Google
-          </Button>
-        </Stack>
-      )}
-    </Flex>
+          <Text mb={4} fontSize="lg" py={4}>
+            <Text as="span" fontWeight="bold" display="inline">
+              Feedback
+            </Text>
+            {`. It's the easiest way to add comments or reviews to your static site. It's still a work-in-progress, but you can try it out by logging in.`}
+          </Text>
+          {auth.user ? (
+            <Button
+              as="a"
+              href="/dashboard"
+              backgroundColor="gray.900"
+              color="white"
+              fontWeight="medium"
+              mt={4}
+              maxW="200px"
+              _hover={{ bg: 'gray.700' }}
+              _active={{
+                bg: 'gray.800',
+                transform: 'scale(0.95)'
+              }}
+            >
+              View Dashboard
+            </Button>
+          ) : (
+            <Stack isInline>
+              <Button
+                onClick={(e) => auth.signinWithGitHub()}
+                backgroundColor="gray.900"
+                color="white"
+                fontWeight="medium"
+                mt={4}
+                _hover={{ bg: 'gray.700' }}
+                _active={{
+                  bg: 'gray.800',
+                  transform: 'scale(0.95)'
+                }}
+              >
+                Sign In with GitHub
+              </Button>
+              <Button
+                onClick={(e) => auth.signinWithGoogle()}
+                backgroundColor="white"
+                color="gray.900"
+                variant="outline"
+                fontWeight="medium"
+                mt={4}
+                _hover={{ bg: 'gray.100' }}
+                _active={{
+                  bg: 'gray.100',
+                  transform: 'scale(0.95)'
+                }}
+              >
+                Sign In with Google
+              </Button>
+            </Stack>
+          )}
+        </Flex>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        width="full"
+        maxWidth="700px"
+        margin="0 auto"
+        mt={8}
+      >
+        <FeedbackLink siteId={SITE_ID} />
+        {allFeedback.map((feedback) => (
+          <Feedback key={feedback.id} {...feedback} />
+        ))}
+      </Box>
+    </>
   );
 };
 
