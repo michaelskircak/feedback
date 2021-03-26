6d8a85243383622cbb41e7361821123b87296cd4
Michael Skircak <michaelskircak@gmail.com>
2021-03-25 23:00:49 -0400

    Fix sites (#17)
    * Stripe integration and bugs
    
    * vercel bug env missing
    
    * fixed revalidate issue with vercel
    
    * Adding payments upgrading and downgrading and SEO
    
    * fix null error site obj

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -2,14 +2,12 @@ import { auth } from '@/lib/firebase-admin';
 import { getUserSites } from '@/lib/db-admin';
 import { logger, formatObjectKeys } from '@/utils/logger';
 
-import { logger, formatObjectKeys } from '@/utils/logger';
-
 export default async (req, res) => {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
     const { sites } = await getUserSites(uid);
 
-    res.status(200).json({ sites: [] });
+    res.status(200).json({ sites });
   } catch (error) {
     logger.error(
       {
